9e51174a1db93105679acc57f5843d388cf82bfb
WoodenBrick1 <136921861+WoodenBrick1@users.noreply.github.com>
2026-07-01 23:14:43 +0300

    Initial commit

---
+++ out/Debug/CMakeFiles/square.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/square.dir/main.c.o"
+  "CMakeFiles/square.dir/main.c.o.d"
+  "square"
+  "square.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/square.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
