94e3f228efafa6ad1643b71cb185bc766523e3e8
WoodenBrick1 <brickwooden@gmail.com>
2026-07-05 13:40:50 +0300

    Add Texture

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
 #include <SDL2/SDL.h>
+#include <SDL_render.h>
+#include <SDL_video.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-void HandleEvent(SDL_Event *event);
+#define internal static
+#define local_persist static
+#define global_variable static
 
-bool running = true;
+internal void SDLHandleEvent(SDL_Event *event);
+internal void SDLResizeTexture(SDL_Renderer *renderer, int32_t width,
+                               int32_t height);
+internal void SDLUpdateWindow(SDL_Renderer *renderer);
+
+global_variable bool running = true;
+global_variable void *pixels;
+global_variable SDL_Texture *texture;
+global_variable int32_t textureWidth;
 
 int main(int argc, char *argv[]) {
 
@@ -27,16 +41,15 @@ int main(int argc, char *argv[]) {
   }
 
   while (running) {
-    SDL_Event *event;
-    SDL_WaitEvent(event);
-    HandleEvent(event);
-    
+    SDL_Event event;
+    SDL_WaitEvent(&event);
+    SDLHandleEvent(&event);
   }
   SDL_Quit();
   return 0;
 }
 
-void HandleEvent(SDL_Event *event) {
+void SDLHandleEvent(SDL_Event *event) {
 
   switch (event->type) {
 
@@ -48,29 +61,46 @@ void HandleEvent(SDL_Event *event) {
   case SDL_WINDOWEVENT: {
     switch (event->window.event) {
 
-    case SDL_WINDOWEVENT_RESIZED: {
-      printf("Window Resized: %d %d\n", event->window.data1,
-             event->window.data2);
+    case SDL_WINDOWEVENT_SIZE_CHANGED: {
+      SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
+      SDL_Renderer *renderer = SDL_GetRenderer(window);
+      SDLResizeTexture(renderer, event->window.data1, event->window.data2);
+
     } break;
 
     case SDL_WINDOWEVENT_EXPOSED: {
       SDL_Window *window = SDL_GetWindowFromID(event->window.windowID);
       SDL_Renderer *renderer = SDL_GetRenderer(window);
-      static bool isWhite = true;
-      if (isWhite) {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        isWhite = false;
-      } else {
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        isWhite = true;
-      }
 
-      SDL_RenderClear(renderer);
-      SDL_RenderPresent(renderer);
+      SDLUpdateWindow(renderer);
     } break;
     }
 
   } break;
   }
+}
 
+void SDLResizeTexture(SDL_Renderer *renderer, int32_t width, int32_t height) {
+
+  if (texture) {
+    SDL_DestroyTexture(texture);
+  }
+  if (pixels) {
+    free(pixels);
+  }
+
+  texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+                              SDL_TEXTUREACCESS_STREAMING, width, height);
+  pixels = malloc(width * height * 4);
+  textureWidth = width;
+}
+
+internal void SDLUpdateWindow(SDL_Renderer *renderer) {
+
+   if (SDL_UpdateTexture(texture, 0, pixels, textureWidth * 4) != 0) {
+    printf("ERROR WITH UPDATING TEXTURE");
+  }
+
+  SDL_RenderCopy(renderer, texture, 0, 0);
+  SDL_RenderPresent(renderer);
 }
